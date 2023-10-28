da95f8590c3557cd0c97153472bf87d47034ae10
ut02 <utsavsimplecool@gmail.com>
2023-10-28 13:58:17 +0530

    signup page backend

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors=require('cors')
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+
+const app = express();
+app.use(cors());
+
+const User = require('./models/User');
+
+const userRoutes= require('./routes/user');
+
+app.use(bodyParser.json());
+
+app.use('/User',userRoutes)
+
+
+app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
